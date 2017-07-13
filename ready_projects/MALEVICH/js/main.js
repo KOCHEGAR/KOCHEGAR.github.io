dc135030b1722b30be97d40ffe06f5e98dc88d06
Khabarovskii <alekseydavaydosvidaniya@gmail.com>
2017-07-13 23:07:54 +1000

    some upd

--- ready_projects/MALEVICH/js/main.js
+++ ready_projects/MALEVICH/js/main.js
@@ -1,3 +1,6 @@
+/*! ScrollMagic v2.0.5 | (c) 2015 Jan Paepke (@janpaepke) | license & info: http://scrollmagic.io */
+!function(e,i){"function"==typeof define&&define.amd?define(["ScrollMagic","jquery"],i):"object"==typeof exports?i(require("scrollmagic"),require("jquery")):i(e.ScrollMagic,e.jQuery)}(this,function(e,i){"use strict";e._util.get.elements=function(e){return i(e).toArray()},e._util.addClass=function(e,t){i(e).addClass(t)},e._util.removeClass=function(e,t){i(e).removeClass(t)},i.ScrollMagic=e});
+
 $(document).ready(function() {
 
   var body = $('body'); // тело страницы
@@ -100,64 +103,84 @@ $(document).ready(function() {
     }
   });
 
+  // // отрицательное число или нет
+  // Math.sign = Math.sign || function(x) {
+  //   x = +x; // преобразуем в число
+  //   if (x === 0 || isNaN(x)) {
+  //     return x;
+  //   }
+  //   return x > 0 ? 1 : -1;
+  // };
 
+  // $('.events .events__item').each(function () {
+
+  // })
+
+  $('.events .events__item').each(function(indx, el) {
+    var self = $(this);
+    var itms = eventsItems.length - (indx);
+    self.find('.event__number').text(itms);
+  });
 
   var s = 0;
+  var W_min = 346;
+  var W_max = 985;
   // init controller
-  // var controller = new ScrollMagic.Controller({vertical: false});
+  var controller = new ScrollMagic.Controller({ vertical: false });
+
+  $('.events .events__item').each(function() {
+    var self = $(this);
+    var eventItem = self.find('.event');
+    var eventItemNumber = self.find('.event__number');
+    var eventItemImgWrap = self.find('.event__img-wrap');
+    var eventItemParagraph = self.find('.event__descr');
+
+    var tween = new TweenMax.to(eventItem, 1, {
+      width: '+=634',
+      ease: Power0.easeNone
+    });
+    var tween2 = new TweenMax.to(eventItemImgWrap, 1, {
+      maxHeight: '597px',
+      ease: Power0.easeNone
+    });
+    var tween3 = new TweenMax.to(eventItemParagraph, 1, {
+      top: 105,
+      ease: Back.easeInOut
+    });
+    var tween4 = new TweenMax.to(eventItemNumber, 1, {
+      color: '#818181',
+      ease: Power0.easeNone
+    });
+    // var tween5 = new TweenMax.fromTo(eventItemNumber, 0.5,{
+    //     y:'-100'
+    // },{
+    //     y:'0'
+    // });
+
+    var timeline = new TimelineMax();
+    timeline.add(tween)
+      .add(tween2, '0')
+      .add(tween3, '0')
+      .add(tween4, '0');
+    // .add(tween5,'0');
+
+    var scene = new ScrollMagic.Scene({
+        triggerElement: self,
+        tweenChanges: true,
+        duration: 256,
+        offset: 0,
+        // reverse: false
+      })
+      .setTween(timeline)
+      .addIndicators()
+      .addTo(controller);
+  });
 
 
-
-  // // $('events').
-
-  // $('.events .events__item').each(function () {
-
-
-  //     var blockTween = new TweenMax.to( $('.main-content'), 0.5, {
-  //      left: "-=535",
-
-
-  //    });
-
-  //     // build scene
-  //     var scene = new ScrollMagic.Scene({
-  //       triggerElement: $(this), 
-  //       // duration: 560,
-  //       offset:  $(this).css('maxWidth'),
-
-  //       reverse: false
-  //     })
-  //     .setTween(blockTween)
-  //     .addIndicators() // add indicators (requires plugin)
-  //     .addTo(controller);
-  // });
-  // $('.events .events__item').each(function () {
-
-
-  //     var blockTween = new TweenMax.to( $(this), 0.5, {
-  //      minWidth: "+=634",
-  //      maxWidth: "+=634",
-
-  //    });
-  //     var maxW = $(this).css('maxWidth');
-  //     maxW = maxW.substr(0, maxW.length - 2);
-
-  //     // maxW= maxW-40;
-  //     console.log(maxW);
-  //     // build scene
-  //     var scene = new ScrollMagic.Scene({
-  //       triggerElement: $(this), 
-
-  //       offset:  maxW+'px',
-
-  //       reverse: false
-  //     })
-  //     .setTween(blockTween)
-  //     .addIndicators() // add indicators (requires plugin)
-  //     .addTo(controller);
-  // });
-  // build tween
-
+  // new TweenMax.staggerFromTo('.events__item', 0.5, 
+  //  {opacity:0, backgroundColor:'white'},
+  //  {opacity:1,backgroundColor:'green'} ,
+  //  0.4);
 
 
 
@@ -183,24 +206,6 @@ $(document).ready(function() {
   var scrollLeft = 0;
 
 
-  var ww = 0;
-  var menuWidth = 0;
-
-  var concreteW_min = 346;
-  var concreteW_max = 985;
-
-  var wholeDocWidth = $('.main-content').width() + menu.width();
-
-  var scrlollMax = wholeDocWidth - ww; /// тут экперементируем
-  var scrollMin = 0; /// тут экперементируем
-
-  var ds = scrlollMax - scrollMin;
-  var dh = concreteW_max - concreteW_min;
-
-
-  var sclrar = 2;
-
-
 
   var debugitem = $('<div>', {
     class: 'debug_item',
@@ -215,7 +220,7 @@ $(document).ready(function() {
     },
     offset: {
       top: 0,
-      left: ($(window).width() / 2) - (2 / 2) + 110
+      left: ($(window).width() / 2) - (2 / 2)
     }
   });
   var debugitem2 = debugitem.clone();
@@ -233,14 +238,20 @@ $(document).ready(function() {
 
   var lastScrollLeft = 0;
 
-  eventsItems.each(function() {
-    $(this).addClass('scrolled');
-  });
+  var windowWidth = 0;
+  // eventsItems.each(function () {
+  //   $(this).addClass('scrolled');
+  // });
 
   // esb.css({
   //   'min-width' : '885px',
   //   'max-width' : '885px'
   // });
+
+  // var W_min = 346;
+  // var W_max = 985;
+
+  var s_Max = 0;
 
 
   $(window).on('scroll', function(e) {
@@ -249,10 +260,11 @@ $(document).ready(function() {
     s = scrollLeft;
 
 
-    ww = $(this).width();
+    windowWidth = $(this).width();
     menuWidth = menu.width();
 
-    halfWindow = ww / 2;
+
+    halfWindow = windowWidth / 2;
     rightSideOf_Menu = menu.width();
 
 
@@ -265,47 +277,16 @@ $(document).ready(function() {
       mainContent.addClass('fixed-menu');
     }
 
+    // var leftVal = mContent.css('left');
+    //     leftVal = leftVal.substr(0, leftVal.length - 2);
 
-    // mContent.css({
-    //   'left' : '-'+ (s/sclrar)+'px'
-    // });
-    // mContent_2.css({
-    //   left:  '-'+(s/sclrar)+'px'
-    // });
+    // if (s<150 && lastScrollLeft > s && leftVal !==0) {
 
-
-    // увеличение элементов events__item
-
-    // var contentWidth = $('main-content').width();
-
-    // left_margin_x = s+menuWidth;
-
-    // dx = Math.abs( contentWidth - ww );
-
-    // var p = left_margin_x/dx;
-
-    // myoffset = ww*p;
-    // var res = (myoffset+s+menuWidth)/1.7;
-
-    // console.log(dx);
-    // console.log(p);
-    // console.log(myoffset);
-    // console.log(res);
-
-
-    // console.log('offset ' + s);
-    // console.log('rightSideOf_Menu ' + (s + halfWindow) );
-    // console.log('halfwindow ' + (halfWindow) );
-    var leftVal = mContent.css('left');
-    leftVal = leftVal.substr(0, leftVal.length - 2);
-
-    if (s < 150 && lastScrollLeft > s && leftVal !== 0) {
-
-      mContent.css({ 'left': 0 });
-      eventsItems.each(function() {
-        $(this).removeAttr('offset');
-      });
-    }
+    //     mContent.css({'left':0});
+    //     eventsItems.each(function () {
+    //        $(this).removeAttr('offset');
+    //     });
+    // }
 
     eventsItems.each(function(indx, el) {
       var self = $(this);
@@ -313,226 +294,72 @@ $(document).ready(function() {
       var itemRightSide = self.offset().left + self.width();
 
       // 
-      if ((s + halfWindow + (menuWidth + 40) >= itemRightSide + 28) && (!(self.hasClass('onview')))) {
+      if ((s + halfWindow >= itemOffsetLeft) && (!(self.hasClass('onview')))) {
 
-        // if (indx === 0) {
-        //     esb.css({
-        //       'min-width':'779px',
-        //       'max-width': '779px' 
-        //     }); 
-        //     $('html,body').stop().animate({
-        //       scrollLeft: esb.offset().left + esb.width()+3
-        //     },400);
-        // }
-        // console.log('okokokokook')
-        self.addClass('onview');
-        // setTimeout(function () {
-        //   var pullLeft = Math.ceil(self.offset().left - s-menuWidth);
-        //   // --- если окно браузера сохранило положение скролла 
-        //   // и после перезагрузки подставила его, то обнуляем число
-        //   // на которое должны были сместить mContent_2.
-        //   if (pullLeft < 0) {pullLeft = 0;}
-        //   // ---
-        //   if (!canPullOrNot) {
-
-        //     self.attr({'offset' : pullLeft});
-
-        //     mContent.css({
-        //       'left' : '-=' + pullLeft
-        //     });          
-        //   }
-        // },130);
+        self.css({ 'borderColor': 'green' });
+        // self.css({'opacity': '0'});
+        // self.addClass('onview');
       }
-      // Немного перестраховки 
-      // Убираем случаи, когда доскроллив до певрого элемента,
-      // остаётся погрешность в расчетах сдвига mContent_2 влево.
-      // Когда достигаем первого элемента , сносим left в 0
-      // if ( (s+halfWindow > itemRightSide-456 && 
-      //       s+halfWindow < itemRightSide-128) &&
-      //       lastScrollLeft > s &&
-      //       self.hasClass('onview') &&
-      //       eventsItems.length-1 === indx ) {
-      //     self.removeAttr('offset');
-
-      //     mContent.css({'left' : 0}); 
-      // }
-
-      // if (s<150 && lastScrollLeft > s && leftVal !==0) {
-      //     console.log('kokook');
-      //     mContent.css({'left':0});
-      //     eventsItems.each(function () {
-      //        $(this).removeAttr('offset');
-      //     });
-      // }
-
-      // if ( (s+halfWindow > itemRightSide-356 && 
-      //       s+halfWindow < itemRightSide-128) && 
-      //       lastScrollLeft > s && 
-      //       self.hasClass('scrolled') && 
-      //       self.hasClass('onview')) {
-
-      //       self.removeClass('scrolled');
-
-      //       if (!canPullOrNot) {
-
-      //         var pullRight = self.attr('offset') || 0;
-      //         var ofst = self.attr('offset');
-
-      //         if ($('.events__item.scrolled').length === 1 || 
-      //             $('.events__item.scrolled').length === 0 ) {
-      //             // Случается что при быстром сдвиге скроллбара влево
-      //             // (если он был прокручен до этого вправо) 
-      //             // возникает ошибка расчетах сдвига mContent_2 вправо
-      //             // и поэтому то же,что и в else if ниже, но при достижении
-      //             // предпоследнего или последнего элемента.
-
-      //             mContent.css('left', '0px');
-
-      //             eventsItems.each(function () {
-      //                $(this).removeAttr('offset');
-      //             });
-      //         }
-      //         else if ( ((+leftVal) + (+ofst)) > 0 ) { 
-      //           // Если проскролить скроллбар слишком быстро вправо
-      //           // (еще это бывает при перезагрузки страницы, 
-      //           // а позиция скролла сохранена браузером и подставлена после перезагрузки)
-      //           // то будут погрешности в расчетах сдвига mContent_2 влево.
-      //           // Поэтому, если leftval+ofst > 0, то выставляем mContent_2.css('left', '0px')
-      //           //
-      //             mContent.css('left', '0px');
-
-      //             eventsItems.each(function () {
-      //                $(this).removeAttr('offset');
-      //             });
-      //         }
-      //         else{  
-      //           // Есди всё ОК, то двигаем
-      //           // self.removeAttr('offset');
-      //           mContent.css({
-
-      //             'left' : '+=' + pullRight
-      //           });
-      //         }
-
-      //       }
-      // }
 
 
-      // отчасти рабочий вариант 1 
+      // отчасти рабочий вариант 
+      if ((s + halfWindow >= itemRightSide + 28 - 346 && self.hasClass('onview')) /*&& (itemRightSide > s+rightSideOf_Menu+400)*/ ) {
 
-      // if ( (s+halfWindow >= itemRightSide && self.hasClass('onview')) /*&& (itemRightSide > s+rightSideOf_Menu)*/  ) {
-
-      //     var minW = self.css('minWidth');
-      //     var maxW = self.css('maxWidth');
-      //     maxW =   maxW.substr(0, maxW.length - 2);
-      //     minW =   minW.substr(0, minW.length - 2);
-
-      //     var sclr = 1.051912313;
-
-      //     if (maxW*sclr > 985) {
-      //         maxW = 985;
-      //         sclr = 1;
-      //     }
-      //       console.log(maxW);
-
-      //       self.css({
-      //         'min-width': maxW * sclr ,
-      //         'max-width': maxW * sclr
-      //       });
-      // }   
+        self.css({ 'borderColor': 'red' });
 
 
-      // console.log('pageScrollLeft 1--- ' + s);
+        //     console.log();
 
-      // отчасти рабочий вариант 2 
-      // if ( (s+halfWindow >= itemRightSide && self.hasClass('onview')) /*&& (itemRightSide > s+rightSideOf_Menu+400)*/  ) {
+        //     // var ds = s_Max - S_min;
+        //     // var dh = W_max - W_min;
 
-      //           var W_min = 346;
-      //           var W_max = 985;
+        //     var S_min = 108+menuWidth*2;  // норм значение
+        //     var q = s;
+        //     if (!self.attr('offset')) {
 
-      //           var wholeDocWidth = $('.main-content').width() + menu.width();
+        //       self.attr('offset', s);
 
-      //           var s_Min = itemOffsetLeft-menuWidth;  /// тут экперементируем
-      //           var s_Max = 0;//wholeDocWidth - $(window).width(); /// тут экперементируем
+        //     }
+        //     var startFrom =  self.attr('offset');
 
-      //           var ds = s_Max - s_Min;
-      //           var dh = W_max - W_min;
-      // // var h = W_max - (( W_max - W_min ) * s) / ( wholeDocWidth - $(window).width() );// уменьшение блока
+        //     if ( q > 0 && lastScrollLeft < s && q <= startFrom ) {
+        //        q = 0;
+        //     }else{
+        //       q -= startFrom;
+        //       if (q <= 0) {
+        //         q = 0;
+        //       }
+        //     }
+        //     if (s+menuWidth >= itemOffsetLeft) { console.log('kokokko'); }
+        //     console.log('currentSroll ' + s + 'px');
 
+        //     // var h = W_max - (( W_max - W_min ) * s) / ( wholeDocWidth - $(window).width() );// уменьшение блока
 
-      // // var h = ( (W_max - W_min) * s ) / (s_Max - s_Min)+W_min; // увеличение блока
+        //     // var h = ( (W_max - W_min) * s ) / (s_Max - S_min)+W_min; // увеличение блока
 
-      // var h = ( ((W_max - W_min) * s ) / s_Min) + W_min; // увеличение блока
-
-
-      //           //прибавляя В_мин в конце, тем самым задаётся начальная ширина элемента,если s равно 0 
-      //           // Math.abs(itemOffsetLeft - s - menuWidth) // способ узнать достижение левой стороны блока края меню
-      //           console.log('resultWidth --- ' + h);
-      //           console.log('indx --- ' + indx);
-      //           console.log('eventsItems.length --- ' + eventsItems.length);
-      //           console.log('pageScrollLeft --- ' + s);
-      //           console.log('itemOffsetLeft --- ' + itemOffsetLeft);
-      //           // console.log('itemOffsetLeft - pageScrollLeft - menuWidth --- ' + (  ) );
-
-      //           console.log('itemRightSide --- ' + itemRightSide);
-      //           console.log('s_Min --- ' + s_Min);
-      //           // console.log('s_Max --- ' + s_Max);
-      //           console.log('вся ширина документа минус ширина экрана --- ' + (wholeDocWidth - $(window).width()) );
-      //           // console.log('оффсет правой стороны элемента --- ' + itemRightSide);
-      //           console.log(' --------------------------------- ');
+        //     var w = ( ((W_max - W_min) * q ) / S_min) + W_min; // увеличение блока
 
 
-      //           if (h > 985) {
-      //             // console.log('kokokoooo;');
-      //             h=985;
-      //             self.css({
-      //               'min-width': h ,
-      //               'max-width': h
-      //             });
-      //               console.log('в ИФ условии resultWidth --- ' + h);
-
-      //           }else{
-      //             self.css({
-      //               'min-width': h ,
-      //               'max-width': h
-      //             });
-      //           }
-
-      // }
+        //     // //прибавляя В_мин в конце, тем самым задаётся начальная ширина элемента,если q равно 0 
 
 
+        //     if (w > W_max) {
+        //       // console.log('kokokoooo;');
+        //       w = W_max;
+        //       self.css({
+        //         'min-width': w ,
+        //         'max-width': w
+        //       });
+        //         console.log('в ИФ условии resultWidth --- ' + w);
 
+        //     }else{
+        //       self.css({
+        //         'min-width': w ,
+        //         'max-width': w
+        //       });
+        //     }
 
-
-
-      //////////////////////////////////////////////////////////////////
-      /// первый вариант не подошел(оставим для чего нибудь дрругого)///
-      //////////////////////////////////////////////////////////////////
-      // if ( s+halfWindow >= itemRightSide /*&& 
-      //      s+menuWidth <= itemRightSide - someOffset*/ ) {
-
-      //     if ( !(self.hasClass('onview')) ) {
-
-      //         // if (indx <= eventsItems.length - (eventsItems.length-5) ) {
-
-      //         //   someOffset+=290;
-      //         // }
-      //         // else{someOffset=0;}
-      //         console.log('offs--1 ' + someOffset);
-      //         self.addClass('onview');
-      //     }
-      // }
-      // else if ( (s+menuWidth)+ww < itemRightSide+menuWidth && self.hasClass('onview')){
-
-      //         // if (indx <= eventsItems.length - (eventsItems.length-5) ) {
-      //         //   // console.log('kokoko222222');
-      //         //   someOffset-=290;
-      //         // }
-      //         // else{someOffset=0;}
-
-      //         console.log('offs--2 ' + someOffset);
-      //         self.removeClass('onview');
-      // }
+      }
     });
 
 
